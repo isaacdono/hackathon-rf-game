1f40cc8f6e222f625a0924648888c4f8085ac405
Miguel Ramos <m281335@dac.unicamp.br>
2025-05-31 14:31:01 -0300

    fix: callback on instance

--- bubbly-fish/main.py
+++ bubbly-fish/main.py
@@ -3,6 +3,9 @@ from serial.serial_reader import SerialReader
 
 if __name__ == "__main__":
     game = Game()
-    serial = SerialReader()
-
+    serial = SerialReader(game.handle_sensor)
+    
+    serial.start_listening()
     game.run()
+
+    serial.stop_listening()
